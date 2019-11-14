ccf4d69353611c958b208ff71f6fe6127944b014
Baptajck <parville.baptiste@gmail.com>
2019-11-14 14:08:12 +0100

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './app.scss';
+import Navigation from 'src/components/Navigation';
+// == Composant
+const App = () => (
+  <div id="app">
+    <Navigation />
+  </div>
+);
+
+// == Export
+export default App;
