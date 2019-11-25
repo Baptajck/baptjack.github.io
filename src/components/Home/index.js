7d9bd3db3d6bc83a5db87d500b2573bed3ec7edb
Baptajck <parville.baptiste@gmail.com>
2019-11-25 17:36:10 +0100

    Finition de la page home dans le gros

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -24,6 +24,20 @@ const Home = () => (
         <img className="logo-image" src={twitter} alt="" />
       </a>
     </div>
+    <div className="description">
+      <div className="description-desc">
+        <h3 className="description-desc-title">Description</h3>
+        <p className="description-desc-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sit magni amet dolorem! Totam sequi voluptatibus ratione, consequatur facere repudiandae.</p>
+      </div>
+      <div className="description-info">
+        <h3 className="description-info-title">Informations</h3>
+        <ul>
+          <li className="description-info-liste"><span className="description-info-liste--gras">Age :</span> 24 ans</li>
+          <li className="description-info-liste"><span className="description-info-liste--gras">Email :</span> parville.baptiste@gmail.com</li>
+        </ul>
+        <p className="description-info-contact">Pour plus d'informations, veuillez-vous rendre sur la page contact</p>
+      </div>
+    </div>
   </div>
 );
 
